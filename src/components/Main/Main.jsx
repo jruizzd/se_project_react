7710b29f82afaf4b6a3a3d242cce4516ee5caff8
Joe <J_rdavid24@outlook.com>
2025-04-12 21:15:21 -0500

    Rendering and filtering Cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <Main>
       <WeatherCard />
@@ -8,7 +10,15 @@ function Main() {
         <p className="cards__text">
           Today is 75 &deg; F / You may want to wear:
         </p>
-        {/* TODO ADDCARDS */}
+        <ul className="cards__list">
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
+        </ul>
       </section>
     </Main>
   );
