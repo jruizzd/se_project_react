4f291163fdc369dd0ac7135e5d3fb038925013fc
Joe <J_rdavid24@outlook.com>
2025-04-18 10:23:57 -0500

    API interactions 2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,6 +14,7 @@ function Main({ weatherData, handleCardClick }) {
         <ul className="cards__list">
           {defaultClothingItems
             .filter((item) => {
+              console.log("Weather type:", weatherData.type);
               return item.weather === weatherData.type;
             })
             .map((item) => {
